9775bfbc2e2ddc8ef6f0f0e780fb13ad8538920a
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-03-15 22:00:56 -0500

    Added up button

--- app/src/main/java/com/japg/ticseguro/LoginActivity.java
+++ app/src/main/java/com/japg/ticseguro/LoginActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.japg.ticseguro.view.CreateAccountActivity;
+
+import android.content.Intent;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void goCreateAccount(View view) {
-
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/japg/ticseguro/view/CreateAccountActivity.java
+++ app/src/main/java/com/japg/ticseguro/view/CreateAccountActivity.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 
 import com.japg.ticseguro.R;
 
+import android.support.v7.widget.Toolbar;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount),true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
