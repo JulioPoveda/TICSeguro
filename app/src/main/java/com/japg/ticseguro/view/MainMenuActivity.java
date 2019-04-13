f5f43cdef4108bb58a4429bf7deb01ba7f09dbb0
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-04-13 11:22:03 -0500

    Added BottomNavigationView fragments

--- app/src/main/java/com/japg/ticseguro/view/MainMenuActivity.java
+++ app/src/main/java/com/japg/ticseguro/view/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package com.japg.ticseguro.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.japg.ticseguro.R;
 
@@ -11,5 +15,35 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.nav_tips:
+                    selectedFragment = new TipsFragment();
+                    break;
+
+                case R.id.nav_perfil:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
