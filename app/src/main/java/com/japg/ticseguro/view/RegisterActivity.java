653b262e5c57eda0ea08e159d6e1c1df393297c1
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-04-15 20:47:25 -0500

    User cannot enter the app until he/she registers

--- app/src/main/java/com/japg/ticseguro/view/RegisterActivity.java
+++ app/src/main/java/com/japg/ticseguro/view/RegisterActivity.java
@@ -6,19 +6,53 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.japg.ticseguro.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText apodoEditText;
+    private Button registrarseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        apodoEditText = findViewById(R.id.apodoEditText);
+        registrarseButton = findViewById(R.id.registrarseButton);
+
+        apodoEditText.addTextChangedListener(registerTextWatcher);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    private TextWatcher registerTextWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String apodoInput = apodoEditText.getText().toString().trim();
+
+            registrarseButton.setEnabled(!apodoInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     public void registrateClick(View view) {
         Intent mainMenuIntent = new Intent(RegisterActivity.this, MainMenuActivity.class);
