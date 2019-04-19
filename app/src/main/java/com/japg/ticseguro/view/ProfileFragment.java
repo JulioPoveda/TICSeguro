bb38b615364f29b0d9347ac1c892e8ac5dd2d51d
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-04-18 21:40:17 -0500

    User can upload avatar

--- app/src/main/java/com/japg/ticseguro/view/ProfileFragment.java
+++ app/src/main/java/com/japg/ticseguro/view/ProfileFragment.java
@@ -1,21 +1,42 @@
 package com.japg.ticseguro.view;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.japg.ticseguro.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class ProfileFragment extends Fragment {
 
+    public static final int PICK_IMAGE = 1;
+
     TextView userNameTextView;
+    ImageView userAvatar;
+    Uri selectedImageUri;
 
     @Nullable
     @Override
@@ -26,6 +47,18 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+
+        userAvatar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Selecciona una imagen"), PICK_IMAGE);
+            }
+
+        });
     }
 
     @Override
@@ -38,5 +71,29 @@ public class ProfileFragment extends Fragment {
         userNameTextView = getView().findViewById(R.id.username);
         userNameTextView.setText(user);
 
+        userAvatar = getView().findViewById(R.id.user_avatar);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE)
+        {
+
+            selectedImageUri = data.getData();
+
+            try
+            {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
+                userAvatar.setImageBitmap(bitmap);
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
