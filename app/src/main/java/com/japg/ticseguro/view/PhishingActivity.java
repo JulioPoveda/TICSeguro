e6f0d657460160a878fba138ca6c9805346b4822
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-04-21 15:47:38 -0500

    Added example video

--- app/src/main/java/com/japg/ticseguro/view/PhishingActivity.java
+++ app/src/main/java/com/japg/ticseguro/view/PhishingActivity.java
@@ -1,11 +1,11 @@
 package com.japg.ticseguro.view;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.japg.ticseguro.R;
 
@@ -17,6 +17,15 @@ public class PhishingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phishing);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        VideoView videoView = findViewById(R.id.video_view_phishing);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 
 }
