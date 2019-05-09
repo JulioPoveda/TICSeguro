64bb8c1e8e6ad3ce40335f192a5d7f4e4384f9bc
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-05-08 21:36:50 -0500

    PruebaTusConocimientosPhishing question 1 added and buttons style fix

--- app/src/main/java/com/japg/ticseguro/view/PruebaTusConocimientosPhishingActivity.java
+++ app/src/main/java/com/japg/ticseguro/view/PruebaTusConocimientosPhishingActivity.java
@@ -2,20 +2,41 @@ package com.japg.ticseguro.view;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.japg.ticseguro.R;
 
 public class PruebaTusConocimientosPhishingActivity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener {
 
     boolean alreadyVisitedActivity = false;
+
+    Button botonOpcion1;
+    Button botonOpcion2;
+    Button botonOpcion3;
+    Button botonOpcion4;
+
+    int numeroDeVecesBoton1Presionado = 2;
+    int numeroDeVecesBoton2Presionado = 2;
+    int numeroDeVecesBoton3Presionado = 2;
+    int numeroDeVecesBoton4Presionado = 2;
+
+    private SensorManager sm;
+
+    private float currentAcceleration;
+
+    private float lastAcceleration;
+
+    private float shake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +48,19 @@ public class PruebaTusConocimientosPhishingActivity extends AppCompatActivity im
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
+        botonOpcion1 = findViewById(R.id.phishing_pregunta_1_opcion_1);
+        botonOpcion2 = findViewById(R.id.phishing_pregunta_1_opcion_2);
+        botonOpcion3 = findViewById(R.id.phishing_pregunta_1_opcion_3);
+        botonOpcion4 = findViewById(R.id.phishing_pregunta_1_opcion_4);
+
         checkConnection();
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(sensorListener, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+        shake = 0.00f;
 
     }
 
@@ -101,5 +134,118 @@ public class PruebaTusConocimientosPhishingActivity extends AppCompatActivity im
     public void onNetworkConnectionChanged(boolean isConnected) {
         showInternetConnectionMessage(isConnected);
     }
+
+    public void seleccionarOpcion1(View view)
+    {
+        if (numeroDeVecesBoton1Presionado % 2 == 0)
+        {
+            botonOpcion1.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            botonOpcion2.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion3.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion4.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton1Presionado = 3;
+            numeroDeVecesBoton2Presionado = 2;
+            numeroDeVecesBoton3Presionado = 2;
+            numeroDeVecesBoton4Presionado = 2;
+        }
+        else
+        {
+            botonOpcion1.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton1Presionado = 2;
+        }
+
+    }
+
+    public void seleccionarOpcion2(View view)
+    {
+        if (numeroDeVecesBoton2Presionado % 2 == 0)
+        {
+            botonOpcion2.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            botonOpcion1.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion3.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion4.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton2Presionado = 3;
+            numeroDeVecesBoton1Presionado = 2;
+            numeroDeVecesBoton3Presionado = 2;
+            numeroDeVecesBoton4Presionado = 2;
+        }
+        else
+        {
+            botonOpcion2.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton2Presionado = 2;
+        }
+
+
+    }
+
+    public void seleccionarOpcion3(View view)
+    {
+        if (numeroDeVecesBoton3Presionado % 2 == 0)
+        {
+            botonOpcion3.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            botonOpcion1.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion2.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion4.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton3Presionado = 3;
+            numeroDeVecesBoton1Presionado = 2;
+            numeroDeVecesBoton2Presionado = 2;
+            numeroDeVecesBoton4Presionado = 2;
+        }
+        else
+        {
+            botonOpcion3.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton3Presionado = 2;
+        }
+    }
+
+    public void seleccionarOpcion4(View view)
+    {
+        if (numeroDeVecesBoton4Presionado % 2 == 0)
+        {
+            botonOpcion4.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            botonOpcion1.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion2.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            botonOpcion3.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton4Presionado = 3;
+            numeroDeVecesBoton1Presionado = 2;
+            numeroDeVecesBoton2Presionado = 2;
+            numeroDeVecesBoton3Presionado = 2;
+        }
+        else
+        {
+            botonOpcion4.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            numeroDeVecesBoton4Presionado = 2;
+        }
+    }
+
+    private final SensorEventListener sensorListener = new SensorEventListener()
+    {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            lastAcceleration = currentAcceleration;
+            currentAcceleration = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = currentAcceleration - lastAcceleration;
+            shake = shake * 0.9f + delta;
+
+            if (shake > 12)
+            {
+                botonOpcion3.setBackgroundColor(getResources().getColor(R.color.colorOpcionCorrecta));
+                botonOpcion1.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+                botonOpcion2.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+                botonOpcion4.setBackgroundColor(getResources().getColor(R.color.colorTarjetaModuloAprendizaje));
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
 }
